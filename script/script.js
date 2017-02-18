e90108f2c7a1cb771577583dc51f25e7057ff5f5
Tropiz <tropiz29@gmail.com>
2017-02-18 16:14:41 +0100

    Added folder structure and fixed line height

--- script/script.js
+++ script/script.js
@@ -2,11 +2,15 @@ function renderTime() {
 	var t = new Date();
 	var h = t.getHours();
 	var m = t.getMinutes();
+	var s = t.getSeconds();
 	if (h < 10) {
 		h = "0" + h;
 	}
 	if (m < 10) {
 		m = "0" + m;
+	}
+	if (s < 10) {
+		s = "0" + s;
 	}
 	document.getElementById('time').innerHTML = h + ":" + m;
 }
